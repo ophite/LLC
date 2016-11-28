270a69b5dda9a386c42d86746426e99452db6c71
Iurii Kobernyk <koiu@ciklum.com>
2016-11-28 09:53:03 +0200

    delete dynamic props

--- app/components/pages/react-virtualized/demo/Table.example.js
+++ app/components/pages/react-virtualized/demo/Table.example.js
@@ -19,14 +19,11 @@ export default class TableExample extends Component {
         super(props, context);
 
         this.state = {
-            disableHeader: false,
             headerHeight: 30,
-            height: 270,
-            hideIndexRow: false,
+            height: 500,
             overscanRowCount: 10,
             rowHeight: 40,
             rowCount: 1000,
-            scrollToIndex: undefined,
             sortBy: 'index',
             sortDirection: SortDirection.ASC,
             useDynamicRowHeight: false
@@ -35,22 +32,17 @@ export default class TableExample extends Component {
         this._getRowHeight = this._getRowHeight.bind(this)
         this._headerRenderer = this._headerRenderer.bind(this)
         this._noRowsRenderer = this._noRowsRenderer.bind(this)
-        this._onRowCountChange = this._onRowCountChange.bind(this)
-        this._onScrollToRowChange = this._onScrollToRowChange.bind(this)
         this._rowClassName = this._rowClassName.bind(this)
         this._sort = this._sort.bind(this)
     }
 
     render() {
         const {
-            disableHeader,
             headerHeight,
             height,
-            hideIndexRow,
             overscanRowCount,
             rowHeight,
             rowCount,
-            scrollToIndex,
             sortBy,
             sortDirection,
             useDynamicRowHeight
@@ -72,137 +64,54 @@ export default class TableExample extends Component {
         return (
             <div className={stylesApplication.Body}>
                 <div className={stylesApplication.column}>
-                    <div>
-                        <div>
-                            <label className={styles.checkboxLabel}>
-                                <input
-                                    aria-label='Use dynamic row heights?'
-                                    checked={useDynamicRowHeight}
-                                    className={styles.checkbox}
-                                    type='checkbox'
-                                    onChange={event => this._updateUseDynamicRowHeight(event.target.checked)}
-                                />
-                                Use dynamic row heights?
-                            </label>
-
-                            <label className={styles.checkboxLabel}>
-                                <input
-                                    aria-label='Hide index?'
-                                    checked={hideIndexRow}
-                                    className={styles.checkbox}
-                                    type='checkbox'
-                                    onChange={event => this.setState({ hideIndexRow: event.target.checked })}
-                                />
-                                Hide index?
-                            </label>
-
-                            <label className={styles.checkboxLabel}>
-                                <input
-                                    aria-label='Hide header?'
-                                    checked={disableHeader}
-                                    className={styles.checkbox}
-                                    type='checkbox'
-                                    onChange={event => this.setState({ disableHeader: event.target.checked })}
-                                />
-                                Hide header?
-                            </label>
-                        </div>
-
-                        <InputRow>
-                            <LabeledInput
-                                label='Num rows'
-                                name='rowCount'
-                                onChange={this._onRowCountChange}
-                                value={rowCount}
-                            />
-                            <LabeledInput
-                                label='Scroll to'
-                                name='onScrollToRow'
-                                placeholder='Index...'
-                                onChange={this._onScrollToRowChange}
-                                value={scrollToIndex || ''}
-                            />
-                            <LabeledInput
-                                label='List height'
-                                name='height'
-                                onChange={event => this.setState({ height: parseInt(event.target.value, 10) || 1 })}
-                                value={height}
-                            />
-                            <LabeledInput
-                                disabled={useDynamicRowHeight}
-                                label='Row height'
-                                name='rowHeight'
-                                onChange={event => this.setState({ rowHeight: parseInt(event.target.value, 10) || 1 })}
-                                value={rowHeight}
-                            />
-                            <LabeledInput
-                                label='Header height'
-                                name='headerHeight'
-                                onChange={event => this.setState({ headerHeight: parseInt(event.target.value, 10) || 1 })}
-                                value={headerHeight}
-                            />
-                            <LabeledInput
-                                label='Overscan'
-                                name='overscanRowCount'
-                                onChange={event => this.setState({ overscanRowCount: parseInt(event.target.value, 10) || 0 })}
-                                value={overscanRowCount}
-                            />
-                        </InputRow>
-
-                        <div>
-                            <AutoSizer disableHeight>
-                                {({ width }) => (
-                                    <Table
-                                        ref='Table'
-                                        disableHeader={disableHeader}
-                                        headerClassName={styles.headerColumn}
-                                        headerHeight={headerHeight}
-                                        height={height}
-                                        noRowsRenderer={this._noRowsRenderer}
-                                        overscanRowCount={overscanRowCount}
-                                        rowClassName={this._rowClassName}
-                                        rowHeight={useDynamicRowHeight ? this._getRowHeight : rowHeight}
-                                        rowGetter={rowGetter}
-                                        rowCount={rowCount}
-                                        scrollToIndex={scrollToIndex}
-                                        sort={this._sort}
-                                        sortBy={sortBy}
-                                        sortDirection={sortDirection}
-                                        width={width}
-                                    >
-                                        {!hideIndexRow &&
-                                        <Column
-                                            label='Index'
-                                            cellDataGetter={
+                    <AutoSizer disableHeight>
+                        {({ width }) => (
+                            <Table
+                                ref='Table'
+                                disableHeader={false}
+                                headerClassName={styles.headerColumn}
+                                headerHeight={headerHeight}
+                                height={height}
+                                noRowsRenderer={this._noRowsRenderer}
+                                overscanRowCount={overscanRowCount}
+                                rowClassName={this._rowClassName}
+                                rowHeight={useDynamicRowHeight ? this._getRowHeight : rowHeight}
+                                rowGetter={rowGetter}
+                                rowCount={rowCount}
+                                sort={this._sort}
+                                sortBy={sortBy}
+                                sortDirection={sortDirection}
+                                width={width}
+                            >
+                                <Column
+                                    label='Index'
+                                    cellDataGetter={
                       ({ columnData, dataKey, rowData }) => rowData.index
                     }
-                                            dataKey='index'
-                                            disableSort={!this._isSortEnabled()}
-                                            width={60}
-                                        />
-                                        }
-                                        <Column
-                                            dataKey='name'
-                                            disableSort={!this._isSortEnabled()}
-                                            headerRenderer={this._headerRenderer}
-                                            width={90}
-                                        />
-                                        <Column
-                                            width={210}
-                                            disableSort
-                                            label='The description label is really long so that it will be truncated'
-                                            dataKey='random'
-                                            className={styles.exampleColumn}
-                                            cellRenderer={
+                                    dataKey='index'
+                                    disableSort={!this._isSortEnabled()}
+                                    width={60}
+                                />
+                                <Column
+                                    dataKey='name'
+                                    disableSort={!this._isSortEnabled()}
+                                    headerRenderer={this._headerRenderer}
+                                    width={90}
+                                />
+                                <Column
+                                    width={210}
+                                    disableSort
+                                    label='The description label is really long so that it will be truncated'
+                                    dataKey='random'
+                                    className={styles.exampleColumn}
+                                    cellRenderer={
                     ({ cellData, columnData, dataKey, rowData, rowIndex }) => cellData
                   }
-                                            flexGrow={1}
-                                        />
-                                    </Table>
-                                )}
-                            </AutoSizer>
-                        </div>
-                    </div>
+                                    flexGrow={1}
+                                />
+                            </Table>
+                        )}
+                    </AutoSizer>
                 </div>
             </div>
         )
@@ -218,7 +127,6 @@ export default class TableExample extends Component {
 
     _getRowHeight({ index }) {
         const { list } = this.props;
-
         return this._getDatum(list, index).size
     }
 
@@ -233,8 +141,9 @@ export default class TableExample extends Component {
         return (
             <div>
                 Full Name
-                {sortBy === dataKey &&
-                <SortIndicator sortDirection={sortDirection}/>
+                {
+                    sortBy === dataKey &&
+                    <SortIndicator sortDirection={sortDirection}/>
                 }
             </div>
         )
@@ -255,23 +164,6 @@ export default class TableExample extends Component {
         )
     }
 
-    _onRowCountChange(event) {
-        const rowCount = parseInt(event.target.value, 10) || 0;
-
-        this.setState({ rowCount })
-    }
-
-    _onScrollToRowChange(event) {
-        const { rowCount } = this.state;
-        let scrollToIndex = Math.min(rowCount - 1, parseInt(event.target.value, 10));
-
-        if (isNaN(scrollToIndex)) {
-            scrollToIndex = undefined
-        }
-
-        this.setState({ scrollToIndex })
-    }
-
     _rowClassName({ index }) {
         if (index < 0) {
             return styles.headerRow
@@ -282,11 +174,5 @@ export default class TableExample extends Component {
 
     _sort({ sortBy, sortDirection }) {
         this.setState({ sortBy, sortDirection })
-    }
-
-    _updateUseDynamicRowHeight(value) {
-        this.setState({
-            useDynamicRowHeight: value
-        })
     }
 }
