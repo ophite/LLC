54d8796d4d13ff6c3776661de53921c8ee778352
Iurii Kobernyk <koiu@ciklum.com>
2016-12-06 15:23:43 +0200

    add new items

--- app/components/pages/grid-layout/GridLayoutComponent.js
+++ app/components/pages/grid-layout/GridLayoutComponent.js
@@ -16,59 +16,113 @@ class GridLayoutComponent extends React.Component {
     static defaultProps = {
         className: "layout",
         rowHeight: 30,
+        breakpoints: { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 },
         cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-        initialLayout: generateLayout()
+        initialItems: generateLayout()
     };
 
     state = {
         currentBreakpoint: 'lg',
         mounted: false,
-        layouts: { lg: this.props.initialLayout },
+        newCounter: 0,
+        items: this.props.initialItems,
     };
 
     componentDidMount() {
         this.setState({ mounted: true });
     }
 
-    generateDOM() {
-        return _.map(this.state.layouts.lg, function (l, i) {
-            return (
-                <div key={i} className={l.static ? 'static' : ''}>
-                    {l.static ?
-                        <span className="text"
-                              title="This item is static and cannot be removed or resized.">Static - {i}</span>
-                        : <span className="text">{i}</span>
-                    }
-                </div>);
-        });
-    }
+    createElement = (el) => {
+        var removeStyle = {
+            position: 'absolute',
+            right: '2px',
+            top: 0,
+            cursor: 'pointer'
+        };
+        var i = el.add ? '+' : el.i;
+        return (
+            <div key={i} data-grid={el}>
+                {
+                    el.add ?
+                        (
+                            <span
+                                className="add text"
+                                onClick={this.onAddItem}
+                                title="You can add an item by clicking here, too.">
+                                Add +
+                            </span>
+                        ) : <span className="text">{i}</span>
+                }
+                <span
+                    className="remove"
+                    style={removeStyle}
+                    onClick={this.onRemoveItem.bind(this, i)}>
+                    x
+                </span>
+            </div>
+        );
+    };
 
-    onBreakpointChange = (breakpoint) => {
+    onAddItem = () => {
+        console.log('adding', 'n' + this.state.newCounter);
         this.setState({
-            currentBreakpoint: breakpoint
+            // Add a new item. It must have a unique key!
+            items: this.state.items.concat({
+                i: 'n' + this.state.newCounter,
+                x: this.state.items.length * 2 % (this.state.cols || 12),
+                y: Infinity, // puts it at the bottom
+                w: 2,
+                h: 2
+            }),
+            // Increment the counter to ensure key is always unique.
+            newCounter: this.state.newCounter + 1
+        });
+    };
+
+    generateDOM = () => {
+        // return _.map(this.state.items, function (l, i) {
+        //     return (
+        //         <div key={i}>
+        //             <span className="text">aaa {i}</span>
+        //         </div>);
+        // });
+        return _.map(this.state.items, this.createElement);
+    };
+
+    onBreakpointChange = (breakpoint, cols) => {
+        this.setState({
+            currentBreakpoint: breakpoint,
+            // cols: cols
         });
     };
 
     onLayoutChange = (layout, layouts) => {
         this.props.onLayoutChange(layout, layouts);
+        this.setState({ layout: layout });
+    };
+
+    onRemoveItem = (i) => {
+        console.log('removing', i);
+        this.setState({ items: _.reject(this.state.items, { i: i }) });
     };
 
     onNewLayout = () => {
         this.setState({
-            layouts: { lg: generateLayout() }
+            items: generateLayout()
         });
     };
 
     render() {
         return (
             <div>
+                <button onClick={this.onAddItem}>Add Item</button>
                 <div>Current Breakpoint: {this.state.currentBreakpoint} ({this.props.cols[this.state.currentBreakpoint]}
                     columns)
                 </div>
                 <button onClick={this.onNewLayout}>Generate New Layout</button>
                 <ResponsiveReactGridLayout
                     {...this.props}
-                    layouts={this.state.layouts}
+                    layouts={{lg : this.state.items}}
                     onBreakpointChange={this.onBreakpointChange}
                     onLayoutChange={this.onLayoutChange}
                     // WidthProvider option
@@ -91,8 +145,7 @@ function generateLayout() {
             y: Math.floor(i / 6) * y,
             w: 2,
             h: y,
-            i: i.toString(),
-            static: Math.random() < 0.05
+            i: i.toString()
         };
     });
 }
--- app/containers/grid-layout/GridLayout.container.jsx
+++ app/containers/grid-layout/GridLayout.container.jsx
@@ -38,6 +38,7 @@ class GridLayoutComponentContainer extends React.Component {
     }
 }
 
+
 GridLayoutComponentContainer.propTypes = {};
 const mapStateToProps = (state) => {
     return {};
