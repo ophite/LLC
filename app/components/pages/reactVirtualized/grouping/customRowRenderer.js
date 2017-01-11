2a67cc560c4ebde38f9ffa91e269cf0b4eb1f6e8
Iurii Kobernyk <koiu@ciklum.com>
2017-01-11 23:28:14 +0200

    minor margin left for plus

--- app/components/pages/reactVirtualized/grouping/customRowRenderer.js
+++ app/components/pages/reactVirtualized/grouping/customRowRenderer.js
@@ -4,79 +4,83 @@ import React from 'react'
 /**
  * Default row renderer for Table.
  */
-export default function defaultRowRenderer ({
-  className,
-  columns,
-  index,
-  isScrolling,
-  key,
-  onRowClick,
-  onRowDoubleClick,
-  onRowMouseOver,
-  onRowMouseOut,
-  rowData,
-  style
+export default function defaultRowRenderer({
+    className,
+    columns,
+    index,
+    isScrolling,
+    key,
+    onRowClick,
+    onRowDoubleClick,
+    onRowMouseOver,
+    onRowMouseOut,
+    rowData,
+    style
 }) {
-  const a11yProps = {}
+    const a11yProps = {}
 
-  if (
-    onRowClick ||
-    onRowDoubleClick ||
-    onRowMouseOver ||
-    onRowMouseOut
-  ) {
-    a11yProps['aria-label'] = 'row'
-    a11yProps.role = 'row'
-    a11yProps.tabIndex = 0
+    if (
+        onRowClick ||
+        onRowDoubleClick ||
+        onRowMouseOver ||
+        onRowMouseOut
+    ) {
+        a11yProps['aria-label'] = 'row'
+        a11yProps.role = 'row'
+        a11yProps.tabIndex = 0
 
-    if (onRowClick) {
-      a11yProps.onClick = () => onRowClick({ index })
-    }
-    if (onRowDoubleClick) {
-      a11yProps.onDoubleClick = () => onRowDoubleClick({ index })
-    }
-    if (onRowMouseOut) {
-      a11yProps.onMouseOut = () => onRowMouseOut({ index })
-    }
-    if (onRowMouseOver) {
-      a11yProps.onMouseOver = () => onRowMouseOver({ index })
-    }
-  }
-
-  if(rowData && rowData.sysMeta){
-    var padLeftStyle = {marginLeft: rowData.sysMeta.level * 20 + 'px'};
-  
-    var collapsedSpan = rowData.sysMeta.collapsed  
-        ? (<span style={padLeftStyle} className="row-pointer">+</span>)
-        : (<span style={padLeftStyle} className="row-pointer">-</span>);
- 
-    var toggleRowFunction = function(){
-        if(onRowClick){
-            onRowClick(rowData);
+        if (onRowClick) {
+            a11yProps.onClick = () => onRowClick({ index })
+        }
+        if (onRowDoubleClick) {
+            a11yProps.onDoubleClick = () => onRowDoubleClick({ index })
+        }
+        if (onRowMouseOut) {
+            a11yProps.onMouseOut = () => onRowMouseOut({ index })
+        }
+        if (onRowMouseOver) {
+            a11yProps.onMouseOver = () => onRowMouseOver({ index })
         }
     }
- 
-      return (
+
+    if (rowData && rowData.sysMeta) {
+
+        const MARGIN_LEFT_PLUS = 20;
+        const marginLeftPlus = rowData.sysMeta.level > 0 ? rowData.sysMeta.level * MARGIN_LEFT_PLUS + 10 : 10;
+
+        var padLeftStyle = { marginLeft: marginLeftPlus + 'px' };
+
+        var collapsedSpan = rowData.sysMeta.collapsed
+            ? (<span style={padLeftStyle} className="row-pointer">+</span>)
+            : (<span style={padLeftStyle} className="row-pointer">-</span>);
+
+        var toggleRowFunction = function () {
+            if (onRowClick) {
+                onRowClick(rowData);
+            }
+        }
+
+        return (
+            <div
+                {...a11yProps}
+                className={className}
+                key={key}
+                style={style}
+                onClick={toggleRowFunction}
+            >
+                {collapsedSpan} {rowData.name}
+            </div>
+        )
+    }
+
+    return (
         <div
-          {...a11yProps}
-          className={className}
-          key={key}
-          style={style}
-          onClick={toggleRowFunction}
+            {...a11yProps}
+            className={className}
+            key={key}
+            style={style}
         >
-           {collapsedSpan} {rowData.name}
+            {columns}
         </div>
-      )
-  }
-  
-  return (
-    <div
-      {...a11yProps}
-      className={className}
-      key={key}
-      style={style}
-    >
-      {columns}
-    </div>
-  )
+    )
 }
