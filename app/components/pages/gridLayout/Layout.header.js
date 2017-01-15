a6396e27e3855e1770e56f71fe8362c87f876fff
Iurii Kobernyk <koiu@ciklum.com>
2017-01-15 03:05:13 +0200

    added sticky header and footer

--- app/components/pages/gridLayout/Layout.header.js
+++ app/components/pages/gridLayout/Layout.header.js
@@ -1,39 +1,94 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 
 
 class LayoutHeader extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrollTop: 0,
+        };
+    }
+
+    componentDidMount() {
+        const node = ReactDOM.findDOMNode(this);
+        if (node) {
+            this.resizer = node.nextSibling;
+            node.parentNode.addEventListener('scroll', this.debouncedOnScrollListener);
+        }
+    }
+
+    debouncedOnScrollListener = (event) => {
+        this.setState({
+            scrollTop: event.target.scrollTop
+        });
+        this.resizer.style.bottom = -event.target.scrollTop + 'px';
+    };
+
+    renderHeader = () => {
         const {
-            uuid,
             headerTitle,
-            layoutComponent,
             isFullScreen,
             handleDeleteLayout,
             handleToggleFullScreenLayout
         } = this.props;
 
         const toggleScreenClassName = isFullScreen ? "normal-screen" : "full-screen";
+        const classNameHeader = {
+            'react-grid__header': true,
+            'header-full-screen': isFullScreen,
+            'header-normal-screen': !isFullScreen,
+        };
+        const classNameHeaderResult = classNames(classNameHeader);
+        const { scrollTop }= this.state;
+
+        return (
+            <div className={classNameHeaderResult} style={Object.assign({}, !isFullScreen && {top: scrollTop + 'px'})}>
+                <div className="react-grid__title">{headerTitle}</div>
+                <div>
+                    <span
+                        className="close"
+                        onClick={handleDeleteLayout}>
+                    </span>
+                </div>
+                <div>
+                    <span
+                        className={toggleScreenClassName}
+                        onClick={handleToggleFullScreenLayout}>
+                    </span>
+                </div>
+            </div>
+        );
+    };
+
+    renderFooter = () => {
+        const { isFullScreen } = this.props;
+        if (isFullScreen) {
+            return null;
+        }
+
+        const { scrollTop }= this.state;
+        return (
+            <div className="react-grid__footer" style={{bottom: -scrollTop + 'px'}}>
+            </div>
+        )
+    };
+
+    render() {
+        const {
+            uuid,
+            layoutComponent,
+        } = this.props;
+
         const component = React.cloneElement(layoutComponent, { uuid });
 
         return (
             <div>
-                <div className="react-grid__header">
-                    <div className="react-grid__title">{headerTitle}</div>
-                    <div>
-                        <span
-                            className="close"
-                            onClick={handleDeleteLayout}>
-                        </span>
-                    </div>
-                    <div>
-                        <span
-                            className={toggleScreenClassName}
-                            onClick={handleToggleFullScreenLayout}>
-                        </span>
-                    </div>
-                </div>
+                {this.renderHeader()}
                 {component}
+                {this.renderFooter()}
             </div>
         );
     }
